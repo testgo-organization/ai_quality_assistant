e9bf7f83a4f7a4ea77a786d87204ba31383be1cb
manuelaraujo1511 <manuel.araujo1511@gmail.com>
2025-08-18 13:11:22 -0500

    change configuration by stagind deployment

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,16 @@
 const API_URLS = {
   development: 'http://127.0.0.1:8000/api/v1/',
   staging: 'https://aws-cx-analyzer.testgoai.com/api/v1/',
+  production: 'https://aws-cx-analyzer.testgoai.com/api/v1/',
 };
 
 const AIGO_API_URLS = {
   development: 'http://127.0.0.1:8010',
   staging: 'https://agent-ai-quality.testgoai.com/',
+  production: 'https://agent-ai-quality.testgoai.com/',
 };
 
-const environment = import.meta.env.MODE || 'development';
+const environment = import.meta.env.MODE || 'production';
 
-export const API_BASE_URL = API_URLS[environment as keyof typeof API_URLS] || API_URLS.development;
-export const AIGO_API_BASE_URL = AIGO_API_URLS[environment as keyof typeof AIGO_API_URLS] || AIGO_API_URLS.development;
+export const API_BASE_URL = API_URLS[environment as keyof typeof API_URLS] || API_URLS.staging;
+export const AIGO_API_BASE_URL = AIGO_API_URLS[environment as keyof typeof AIGO_API_URLS] || AIGO_API_URLS.staging;
